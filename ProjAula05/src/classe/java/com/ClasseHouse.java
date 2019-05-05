e876e0f431c43991c2d695c1f7bd09443e695847
DEVanderAJR <49560696+DEVanderAJR@users.noreply.github.com>
2019-05-05 12:42:20 -0300

    Classes em Java - Testes iniciais.

---
+++ ProjAula05/src/classe/java/com/ClasseHouse.java
@@ -0,0 +1,192 @@
+package classe.java.com;
+
+public class ClasseHouse {
+	
+/*
+ * Atributos
+ * */
+	String Color ="";
+	Double Size = 0.0;
+	String Locator ="";
+	Double Price = 0.0;
+	Double ValueHipo =0.0;
+	Double ValueLuz =0.0;
+	Double LimiteEmprestimo = 1000.0;
+	boolean	EmpAtivo = false;
+	private Boolean Quitado = true;
+	private Double Saldo = 0.0;
+	private boolean MoneyTerc = false;
+	private Double ControlEmprestimo = 0.0; 
+	
+
+/*
+ * Comportamentos e métodos
+ * */
+	
+public void ConsultaMoney() {			
+	
+		System.out.println("Saldo: R$" + this.Saldo);
+		
+		if(this.ControlEmprestimo > 0.0) {
+			System.out.println("Empréstimo: R$" + this.ControlEmprestimo);
+		}
+	}
+	
+	
+	public void InMoney(double x) {		
+		
+		this.Saldo += x;
+		System.out.println("+ R$"+ x);
+	}
+	
+	public void OutMoney(double y) {		
+		
+		while((this.Saldo <= y) && this.ControlEmprestimo <= this.LimiteEmprestimo) {
+			
+			this.SolicitaEmprestimo(100, 1);	
+			this.MoneyTerc = true;
+			this.EmpAtivo = this.MoneyTerc;			
+						
+		}	
+		
+		if(this.Saldo >= y) {
+			this.Saldo -= y;
+			System.out.println("- R$"+ y);
+		}
+		else System.out.println("Sem recursos no momento, solicite um emprestimo. Opção 11.");
+		
+	}
+	
+	public void SolicitaEmprestimo(double y, int x) {
+		
+	switch(x) {
+		case 1: //Solicita;			
+			if(this.ControlEmprestimo <= this.LimiteEmprestimo) {	
+					this.InMoney(y);
+					this.ControlEmprestimo += y;
+			}
+			else {
+				
+				System.out.println("Você atingiu seu limite de Crédito: R$"+this.LimiteEmprestimo+"\n. Solicite mais crédito na opção 11.");				
+			}
+					
+			break;
+		case 2: // Abate parcialmente;
+			
+			if(( this.ControlEmprestimo - y) == 0 ) {
+					
+				this.OutMoney(this.ControlEmprestimo);					
+				this.ControlEmprestimo = 0.0;
+				System.out.println("Situação de Empréstimo atual: R$"+ this.ControlEmprestimo +"\n");
+				this.MoneyTerc = false;
+				this.EmpAtivo = this.MoneyTerc;
+			}
+			else if((this.ControlEmprestimo -y) < 0) {		
+						
+				this.OutMoney(this.ControlEmprestimo);//quita emprestimo
+				
+				if( this.ControlEmprestimo > 0.0) {
+					System.out.println("Devolução de excedentes: R$"+ (y - this.ControlEmprestimo));
+					this.InMoney(y - this.ControlEmprestimo);//estorna valor excedente;
+				}
+				
+				this.ControlEmprestimo = 0.0;
+				this.MoneyTerc = false;
+				this.EmpAtivo = this.MoneyTerc;
+				
+				System.out.println("Situação de Empréstimo atual: R$"+ this.ControlEmprestimo +"\n");
+				
+			}		
+			
+			else if((this.ControlEmprestimo -y)> 0) {	
+				
+				this.ControlEmprestimo -= y;				
+				
+				this.OutMoney(y);//abate emprestimo
+				
+				System.out.println("Situação de Empréstimo atual: R$"+ this.ControlEmprestimo +"\n");
+				this.MoneyTerc = false;
+				this.EmpAtivo = this.MoneyTerc;
+			}
+			break;
+		case 3:
+			System.out.println("Situação de Empréstimo atual: R$"+ this.ControlEmprestimo +"\n Saldo atual é:" + this.Saldo);
+			if(this.ControlEmprestimo > 0.0) {
+				
+				this.MoneyTerc = true;
+				this.EmpAtivo = this.MoneyTerc;
+			}
+			break;
+	}
+	
+	}
+	
+	public void OpenGarage() {		
+				
+		System.out.println("Garagem aberta!");
+	}
+	
+	public  void CloseGarage() {
+		
+		System.out.println("Garagem Fechada!");
+	}
+	
+	public  void PintarCasa() {
+		
+		System.out.println(this.Color);
+	}
+	
+	public boolean BillingExpense(int q) {		
+		
+		if( q == 1) {
+				
+			this.Quitado = this.CheckHipoteca();
+		
+		}
+				
+		else if(q == 2) {
+			
+			this.Quitado = this.CheckLuz();
+		}
+				
+		
+	return this.Quitado;
+		
+	}
+	
+	private boolean CheckHipoteca() {
+		
+		if (this.ValueHipo != 0) {
+			
+			System.out.println("Fatura de Hipoteca em aberto no valor de: R$"+ this.ValueHipo);
+			this.OutMoney(this.ValueHipo);
+			this.ValueHipo = 0.0;	
+		
+			return true;
+		}
+		
+		System.out.println("Fatura de Hipoteca em dia: R$"+ this.ValueHipo);
+		
+	return false;
+		
+	}
+	
+private boolean CheckLuz() {
+		
+		if (this.ValueLuz != 0) {
+			
+			System.out.println("Fatura de Luz em aberto no valor de: R$"+ this.ValueLuz);
+			this.OutMoney(this.ValueLuz);
+			this.ValueLuz = 0.0;	
+		
+			return true;
+		}
+		
+		System.out.println("Fatura de Luz em dia: R$"+ this.ValueLuz);
+		
+	return false;
+		
+	}
+		
+	
+}
---
+++ ProjAula05/src/classe/java/com/ClasseMain.java
@@ -0,0 +1,179 @@
+package classe.java.com;
+
+import java.util.Scanner;
+
+public class ClasseMain {
+	
+	static int control = 0;
+	
+	public static void Menu(int x) {
+		 
+		if (x == 1) {
+			System.out.println("\n|||||||||||||||-1- Pintar Casa.");
+			System.out.println("|||||||||||||||-2- Abrir Garagem.");
+			System.out.println("|||||||||||||||-3- Fechar Garagem.");
+			System.out.println("|||||||||||||||-4- Contas a Pagar.");
+			System.out.println("|||||||||||||||-5- Crédito.");
+			System.out.println("|||||||||||||||-6- Débito.");
+			System.out.println("|||||||||||||||-7- Saldo.");
+			System.out.println("|||||||||||||||-0- Sair:\n");
+		}
+		else {
+			System.out.println("\n|||||||||||||||-1- Pintar Casa.");
+			System.out.println("|||||||||||||||-2- Abrir Garagem.");
+			System.out.println("|||||||||||||||-3- Fechar Garagem.");
+			System.out.println("|||||||||||||||-4- Contas a Pagar.");
+			System.out.println("|||||||||||||||-5- Crédito.");
+			System.out.println("|||||||||||||||-6- Débito.");
+			System.out.println("|||||||||||||||-7- Saldo.");
+			System.out.println("|||||||||||||||-6- Débitoo.");
+			System.out.println("|||||||||||||||-7- Saldo.");
+			System.out.println("|||||||||||||||-8- Consulta Emprestimo.");
+			System.out.println("|||||||||||||||-9- Quitar Emprestimo.");
+			System.out.println("|||||||||||||||-10- Solicitar Emprestimo.");
+			System.out.println("|||||||||||||||-11- Alterar Limite de Crédito.");
+			System.out.println("|||||||||||||||-0- Sair:\n");
+			control++;
+		}
+			
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int exit = 0;
+		Double x = 0.0;
+		
+		System.out.println("Instanciando Sistema House.");	
+		System.out.println("|||||||||||||||Bem vindo ao Sistema House - O que deseja fazer? ||||||||||||");
+		
+		ClasseHouse HouseOne = new ClasseHouse();
+		
+		//do {
+		while(exit <= 0) {	
+			
+			if (!HouseOne.EmpAtivo) Menu(1);
+			else Menu(2);
+			
+			Scanner in1 = new Scanner(System.in);
+			
+			
+			switch(in1.nextLine()) {
+				case "1":
+					
+					System.out.println("|||||||||||||||Escolha uma cor abaixo ||||||||||||");
+					System.out.println("|||||||||||||||-1- Vermelho.");
+					System.out.println("|||||||||||||||-2- Azul.");
+					
+					switch(in1.nextLine()) {											
+					case "1":
+						HouseOne.Color="red";
+						HouseOne.PintarCasa();
+						break;
+					case"2":
+						HouseOne.Color="blue";
+						HouseOne.PintarCasa();
+						break;							
+						}	
+					break;
+				case "2":
+						HouseOne.OpenGarage();
+					break;
+				case "3":
+						HouseOne.CloseGarage();
+					break;
+				case "4":
+					System.out.println("|||||||||||||||Escolha uma conta abaixo para Pgto ||||||||||||");
+					System.out.println("|||||||||||||||-1- Luz.");
+					System.out.println("|||||||||||||||-2- Hipoteca.");
+				
+					switch(in1.nextLine()) {											
+						case "1":
+							System.out.println("Digite o Valor da Conta:");
+							HouseOne.ValueLuz = Double.valueOf(in1.nextLine());
+							HouseOne.BillingExpense(2);
+							
+							break;
+						case"2":
+							System.out.println("Digite o Valor da Conta:");
+							HouseOne.ValueHipo = Double.valueOf(in1.nextLine());
+							HouseOne.BillingExpense(1);
+							
+							break;							
+							}	
+					break;			
+				case "5":
+					
+					System.out.println("Digite o Valor para depósito:");
+					HouseOne.InMoney(Double.valueOf(in1.nextLine()));
+					HouseOne.ConsultaMoney();
+										
+					break;
+				case "6":
+					System.out.println("Digite o Valor para saque:");
+					HouseOne.OutMoney(Double.valueOf(in1.nextLine()));
+					HouseOne.ConsultaMoney();
+					break;
+				case "7":
+					HouseOne.ConsultaMoney();
+					break;
+				case "8":
+					
+					if (control > 0) {
+						
+						HouseOne.SolicitaEmprestimo(0.0, 3);
+					}
+					
+					break;
+				case "9":
+					
+					if (control > 0) {
+						
+						System.out.println("Digite o Valor para Armotização:");
+						HouseOne.SolicitaEmprestimo(Double.valueOf(in1.nextLine()), 2 );
+						
+					}
+					
+					
+					break;
+				case "10":
+					
+					if (control > 0) {
+						
+						System.out.println("Digite o Valor da Solicitação:");
+						HouseOne.SolicitaEmprestimo(Double.valueOf(in1.nextLine()), 1 );					
+						
+					}
+					
+					break;
+				case "11":
+					
+					if (control > 0) {						
+
+						System.out.println("Como deseja alterar seu crédito atual? R$"+ HouseOne.LimiteEmprestimo);
+						x = Double.valueOf(in1.nextLine());
+						
+						if(HouseOne.LimiteEmprestimo != x) {
+							HouseOne.LimiteEmprestimo = x;	
+						}
+						
+					}
+					break;
+				case "0":
+					System.out.println("Até mais.");
+					exit = 1;
+					break;
+				
+				
+			default: exit = 1 ;
+			}
+								
+		
+		}//while(exit> 0);
+		
+		
+		
+			
+	}
+
+}
